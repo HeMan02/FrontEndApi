65054f9290314aa922da1a8d2e9320d5e6bc82e1
Wilson <musso.luca89@gmail.com>
2023-04-07 14:38:14 +0200

    charts + excel

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import excel from 'vue-excel-export'
+import vueApexChats from 'vue-apexcharts'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  excel,
+  vueApexChats,
   render: h => h(App)
 }).$mount('#app')
