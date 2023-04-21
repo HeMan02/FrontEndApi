401e9e8fa5d8cf84993f11a0794131536e4e49d4
Wilson <musso.luca89@gmail.com>
2023-04-21 15:09:32 +0200

    no message

---
+++ src/card/MainCardNavigation.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card class="mx-auto" max-width="344" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">{{ title }}</div>
+        <v-list-item-title class="text-h5 mb-1">
+          {{ subtitle }}
+        </v-list-item-title>
+        <v-text-field label="Main input" hide-details="auto"> </v-text-field>
+      </v-list-item-content>
+      <v-list-item-avatar tile size="80">
+        <v-img v-bind:src="require('../assets/' + icon)"> </v-img>
+      </v-list-item-avatar>
+    </v-list-item>
+    <v-card-actions>
+      <v-btn color="blue" outlined rounded text>GO</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    subtitle: String,
+    icon: String,
+  },
+};
+</script>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,7 +1,60 @@
 <template>
-  <div id="app">
-    <v-app id="inspire">
+  <div>
+    <v-app>
       <h1>This is an about page</h1>
+      <v-row>
+        <v-col>
+          <MainCardNavigation
+            title="Level 1"
+            subtitle="Sub Level 1"
+            icon="test"
+          />
+        </v-col>
+        <v-col>
+          <v-card class="mx-auto" max-width="344" outlined>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="text-overline mb-4">OVERLINE</div>
+                <v-list-item-title class="text-h5 mb-1">
+                  Headline 5
+                </v-list-item-title>
+                <v-list-item-subtitle
+                  >Greyhound divisely hello coldly
+                  fonwderfully</v-list-item-subtitle
+                >
+              </v-list-item-content>
+
+              <v-list-item-avatar
+                tile
+                size="80"
+                color="grey"
+              ></v-list-item-avatar>
+            </v-list-item>
+
+            <v-card-actions>
+              <v-btn outlined rounded text> Button </v-btn>
+            </v-card-actions>
+          </v-card></v-col
+        >
+        <v-col> <CardTest /> </v-col>
+      </v-row>
     </v-app>
   </div>
 </template>
+
+<script>
+import MainCardNavigation from "@/card/MainCardNavigation.vue";
+import CardTest from "@/card/CardTest.vue";
+export default {
+  components: {
+    MainCardNavigation,
+    CardTest,
+  },
+
+  props: {
+    title: String,
+    subtitle: String,
+    icon: String,
+  },
+};
+</script>
