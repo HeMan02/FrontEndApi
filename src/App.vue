4b24aa1c9a6f7f6002daaac46fe0b5183cb51438
Wilson <musso.luca89@gmail.com>
2023-04-21 11:57:48 +0200

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,42 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <div id="app">
+    <v-app id="inspire">
+      <div>
+        <v-app-bar color="deep-purple accent-4" dense dark>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+          <v-toolbar-title>Page title</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon @click="GoToMainPage">
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-menu left bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon v-bind="attrs" v-on="on">
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+                <v-list-item-title>Option {{ n }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-app-bar>
+        <v-main>
+          <router-view />
+        </v-main>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn target="_blank" text @click="GoToMainPage">
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+    </v-app>
+  </div>
 </template>
 
 <script>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="app">
+    <v-app id="inspire">
+      <h1>This is an about page</h1>
+    </v-app>
   </div>
 </template>
